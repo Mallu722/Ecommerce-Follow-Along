9830399dedaba42109c414c5e8da7566a9e33807
Mallikarjun722 <anandhiremath979@gmail.com>
2025-01-14 17:04:13 +0530

    Your descriptive commit message

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const app = express();
-const user = require("./controller/user");
 
-app.use("/api/v2/user",user);
+if(process.env.NODE_EMV !== "PRODUCTION"){
+    require("dotenv").config({
+        path: "backend/config/.env",
+    })
+}
 
-if(process.env.NODE_ENV !== 'PRODUCTION') {
-    require('dotenv').config({
-        path:"Backend/config/.env",
-    });
-
-};
-
-app.get('/',(req,res) =>{
-    return res.send('Welcome to backend');
-});
+app.get('/', (req, res)=>{
+    return res.send("Welcome to backend");
+})
 
 module.exports = app;
--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,29 +1,27 @@
 const express = require("express");
-const path = require("path");
-const User = require("..model/user");
+const path = require("path")
+
+const User = require("../model/userModel");
 const router = express.Router();
-const {upload} = require("../multer");
-const ErrorHandler = require("../utils/errorHandler");
+const {upload} = require("../middleware/multer");
+const ErrorHandler = require("../utils/ErrorHandler");
 
 
-//create user
-
-router.post("/create-user", upload.single("image"), async (req, res) => {
-    const {name, email, password, role} = req.body;
-    const userEmail = await User.findOne({email});
+router.post("/register", upload.single("image"), async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const userEmail = await User.findOne({ email });
     if (userEmail) {
-        return nextTick(new ErrorHandler("user already exist", 400));
-        }
-        
+        return next(new ErrorHandler("User already exist", 400));
+    }
 const filename = req.file.filename;
-const fileUrl = path.join(filename);
-const user = {
-    name:name,
-    email:email,
-    password:password,
-    avatar:fileUrl,
-};
-console.log(user);
+const fileUrl = path.join("uploads", filename);
+    const user = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: fileUrl,
+    };
+    console.log(user);
 });
 
-module.exports=router;
+module.exports = router;
