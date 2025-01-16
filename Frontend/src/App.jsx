dcde0a85c97f181fff338430727dd55b050620f7
Mallikarjun722 <anandhiremath979@gmail.com>
2025-01-16 13:40:45 +0530

    change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 // eslint-dis4able-next-line
 import {BrowserRouter , Routes, Route } from 'react-router-dom';
 import {LoginPage} from './Routes/routes.js';
+import {SignupPage} from './Routes/routes.js';
+
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage/>} />
     </Routes>
     </BrowserRouter>  
   )
