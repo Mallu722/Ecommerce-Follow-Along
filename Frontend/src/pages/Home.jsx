985b2de0c13a7f0a8a21649e1711ec794a3e5379
Mallikarjun722 <anandhiremath979@gmail.com>
2025-01-24 13:06:30 +0530

    change

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const ProductDetails = [
     },
     {
         name: "Product 2",
-        image: "https://media.istockphoto.com/id/1319625327/photo/sh",
+        image:"https://media.istockphoto.com/id/1319625327/photo/shopping-basket-full-of-variety-of-grocery-products-food-and-drink-on-yellow-background.jpg?s=612x612&w=0&k=20&c=GHyTjlkoFweJnbAadmn4tzEYvfiB73MTe93KMT3GIM0=",
         description: "This is a limeted-time product offering.",
         price: 200,
     },
@@ -63,7 +63,7 @@ const ProductDetails = [
     },
 ];
 
-export default function Home () {
+export default function Home() {
     return (
         <div className="w-full min-h-screen bg-neutral-800">
             <div className="grid grid-cols-5 gap-4 p-4">
