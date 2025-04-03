38af2d7ed435dd5a55c17a6a99dd1555700ef08d
Mallu722 <anandhiremath979@gmail.com>
2025-04-03 09:48:45 +0530

    Update user.js

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,4 +1,4 @@
-const User = require("../model/modal");
+const User = require("../model/user");
 const express = require("express");
 const path = require("path")
 const fs = require("fs");
@@ -9,6 +9,9 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const { default: mongoose } = require("mongoose");
+const { type } = require("os");
+const { count } = require("console");
 require("dotenv").config();
 
 // create user
@@ -26,7 +29,7 @@ router.post("/create-user", upload.single("file"), catchAsyncErrors( async (req,
                 console.log("Error removing file:",err);
                 return res.status(500).json({message:"Error removing file"});
             }
-
+    
         }
 
         return next(new ErrorHandler("User already exists", 400));
@@ -39,58 +42,129 @@ router.post("/create-user", upload.single("file"), catchAsyncErrors( async (req,
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log("At Create", "Password:", password, "Hash:", )
     const user = await User.create({
-        name: name,
-        email: email,
-        password: hashedPassword,
+        name,
+        email,
+        password,
         avatar:{
             public_id: req.file?.filename || "",
             url: fileUrl ,
         },
     });
-
-    console.log(user)
-    res.status(201).json({ success : true , user });
+    
+console.log(user)
+res.status(201).json({ success : true , user });
 }));
 
+router.post("/login-user", catchAsyncErrors(async(req, res, next) =>{
+    console.log("Logging in user...");
 
+    let{email , password} = req.body;
+    email = email;
+    password= password;
+    if (!email || !password){
+        return next(new ErrorHandler("Please enter both email and password", 400));
+    }
+    const user_authen = await User.findOne({ email }).select("+password");
+    if (!user_authen) {
+        console.log("Invalid email or password");
+        return next(new ErrorHandler("No such email found. Please register first", 401));
+    }
+    const isPasswordMatched = await bcrypt.compare(password, user_authen.password);
+    
+    console.log("Passoword matched result:", isPasswordMatched );
 
-router.post('/login-user',catchAsyncErrors(async (req,res,next)=>{
-    console.log("Logging in user...")
+    console.log("At Auth- password", password, "Hash:", user_authen.password );
 
-    let {email , password}=req.body
-
-    if(!email || !password){
-        return next(new ErrorHandler("Provide both email and password",400))
+    if (!isPasswordMatched) {
+        console.log("Password mismatch");
+        return next(new ErrorHandler("Authentication failed , Invalid password.", 401));
     }
 
-    const user_auth=await User.findOne({email}).select('+password')
+    res.status(200).json({
+        success : true ,
+        message : "Login successfully.",
+        user : {
+            id : user_authen._id ,
+            email : user_authen.email ,
+            name : user_authen.name ,
+        },
+        });
+}));
 
-    if(!user_auth){
-        return next(new ErrorHandler("Please signup as we dont have ur account",401))
+router.get("/Profile", catchAsyncErrors(async(req,res,next) => {
+    const {email} = req.query;
+
+    if(!email) {
+        return next(new ErrorHandler("Please provide an email", 400));
     }
 
-    const isPasswordMatched=await bcrypt.compare(password, user_auth.password);
-    console.log("Matched ?",isPasswordMatched)
-
-
-
-    if (!isPasswordMatched){
-        console.log("Password not match")
-        return next(new ErrorHandler("Invalid password",401))
+    const user = await User.findOne({email});
+    if(!user) {
+        return next(new ErrorHandler("User not found", 404));
     }
+
     res.status(200).json({
         success:true,
-        message:"Login successful",
         user:{
-            id: user_auth.id,
-            name: user_auth.name
-        }
-    }
-    );
-
+            name: user.name,
+            email: user.email,
+            phone: user.phoneNumber,
+            avatarUrl: user.avatar.url
+        },
+        addresses: user.addresses,
+    });
 }))
 
+router.post("/add-address", catchAsyncErrors(async (req,res,next) => {
 
+    console.log("Received address data:", req.body);
+    const {country, city, address1, address2, zipCode, addressType, email} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(!user) {
+        return next(new ErrorHandler("User not found", 404));
+    }
+
+    const newAddress = {
+        country,
+        city,
+        address1,
+        address2,
+        zipCode,
+        addressType,
+    };
+
+    user.addresses.push(newAddress);
+    await user.save();
+
+    res.status(201).json({
+        success: true,
+        addresses: user.addresses,
+    });
+}))
+
+router.get("/addresses", catchAsyncErrors(async(req, res, next) => {
+    console.log("Fetching addresses for:", req.query.email);
+
+    const {email} = req.query;
+    if(!email) {
+        console.log("Email not provided");
+        return next(new ErrorHandler("Please provide an email", 400));
+    }
+
+    const user = await User.findOne({email});
+    if(!user) {
+        console.log("User not found in DB");
+        return next (new ErrorHandler("User not found", 404));
+    }
+
+    console.log("User found:", user);
+    res.status(200).json({
+        success: true,
+        addresses: user.addresses,
+    });
+}));
 
 
 module.exports = router;
